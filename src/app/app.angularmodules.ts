4c2631a811a7b950b30f14ffa61ed9648cb883bb
uditrpanchal <uditrpanchal@gmail.com>
2018-03-28 16:17:50 -0400

    node-api-server with POST request

--- src/app/app.angularmodules.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { MatButtonModule, MatToolbarModule, MatIconModule, MatInputModule, MatFormFieldModule, MatTableModule, MatStepperModule, MatCardModule, MatListModule, MatSelectModule, MatDialogModule, MatSlideToggleModule, MatTabsModule, MatDatepickerModule, MatNativeDateModule, MatExpansionModule, MatCheckboxModule, MatRadioModule } from '@angular/material';
-
-@NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatInputModule, MatFormFieldModule, MatTableModule, MatStepperModule, MatCardModule, MatListModule, MatSelectModule, MatDialogModule, MatSlideToggleModule, MatTabsModule, MatDatepickerModule, MatNativeDateModule, MatExpansionModule, MatCheckboxModule, MatRadioModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatInputModule, MatFormFieldModule, MatTableModule, MatStepperModule, MatCardModule, MatListModule, MatSelectModule, MatDialogModule, MatSlideToggleModule, MatTabsModule, MatDatepickerModule, MatNativeDateModule, MatExpansionModule, MatCheckboxModule, MatRadioModule],
-})
-
-export class AngularMaterialModules { }
